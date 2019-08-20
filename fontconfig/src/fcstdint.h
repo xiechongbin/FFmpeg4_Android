4c97b942f397813d8053f9248e4bbac32f1f9fe6
xcb <chongbinxcb@126.com>
2019-08-19 23:20:24 -0700

    fontconfig delete some compile produced files

--- fontconfig/src/fcstdint.h
+++
@@ -1,9 +0,0 @@
-#ifndef _FONTCONFIG_SRC_FCSTDINT_H
-#define _FONTCONFIG_SRC_FCSTDINT_H 1
-#ifndef _GENERATED_STDINT_H
-#define _GENERATED_STDINT_H "fontconfig 2.13.91"
-/* generated using gnu compiler x86_64-linux-android-gcc (GCC) 4.9.x 20150123 (prerelease) */
-#define _STDINT_HAVE_STDINT_H 1
-#include <stdint.h>
-#endif
-#endif
